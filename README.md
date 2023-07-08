355fa2e8da98a800f9469671dc87137f39572579
chime <chrisnnchris@gmail.com>
2023-07-08 01:05:09 -0700

    Update readme and first UI test

--- README.md
+++ README.md
@@ -7,13 +7,19 @@ For Ubuntu, use command
 Python
 pytest
 dotenv
+Chrome
+Chromedriver
+
 2) Create environmental file
 
 3) Command for running all tests
-python3 -m pytest -s --html=reporter.html
+"python3 -m pytest -s --html=reporter.html"
 4) Command for running single test
-"python3 -m pytest -s tests/test_api.py --html=reporter.html"
+"python3 -m pytest -s tests/test_finnHubQuote.py --html=reporter.html"
+GUI tests
+"python3 -m pytest -s tests/test_website.py --html=reporter.html"
 
+-s flag for displaying console messages
 
 Reporter via pytest-html
 
--- tests/test_website.py
+++ tests/test_website.py
@@ -1,8 +1,10 @@
 import pytest
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # A random website that shows a joke
-WEB_URL = "https://jokes.one/"
+WEB_URL = "https://finnhub.io/"
 
 # A fixture that creates and returns a webdriver instance
 @pytest.fixture(scope="module")
@@ -12,10 +14,30 @@ def driver():
     driver.quit()
 
 # A test that checks if the website title is correct
-def test_title(driver):
+def test_APIKeyGen(driver):
     driver.get(WEB_URL)
-    assert driver.title == "Jokes One - Jokes, Funny Videos, Funny Pictures"
+    getAPIKeyBtn = driver.find_element(By.XPATH, ".//a[contains(text(), 'Get free API key') ]")    
+    assert getAPIKeyBtn.text == "Get free API key"
+    getAPIKeyBtn.click()
+    signUpH2Element = driver.find_element(By.XPATH, ".//h2")
+    print(signUpH2Element.text)
+    assert signUpH2Element.text == "Welcome to Finnhub.io"
+    signupInputs = driver.find_elements(By.TAG_NAME, "input")    
+    print(len(signupInputs))
+    assert len(signupInputs) == 3
+    print(signupInputs[0])
+    nameInput = signupInputs[0].get_attribute("placeholder")
+    print(nameInput)
+    assert nameInput == "Name"
+    emailInput = signupInputs[1].get_attribute("placeholder")
+    print(emailInput)
+    assert emailInput == "Email"
+    passwordInput = signupInputs[2].get_attribute("placeholder")
+    print(passwordInput)
+    assert passwordInput == "Password"
 
+    #assert driver.title == "Jokes One - Jokes, Funny Videos, Funny Pictures"
+'''
 # A test that checks if the website has a joke section
 def test_joke_section(driver):
     driver.get(WEB_URL)
@@ -27,3 +49,4 @@ def test_random_joke_button(driver):
     driver.get(WEB_URL)
     random_joke_button = driver.find_element_by_id("random-joke-button")
     assert random_joke_button.is_displayed()
+'''
